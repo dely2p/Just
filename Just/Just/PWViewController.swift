e62fde45bb1a9f05f334a442523f4d23412c74c9
dely <lhyal2p@gmail.com>
2018-05-16 23:49:39 +0900

    add TextField to input password
    암호 입력을 할 수 있는 textField를 추가함

---
+++ Just/Just/PWViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PWViewController.swift
+//  Just
+//
+//  Created by dely on 2018. 5. 16..
+//  Copyright © 2018년 dely. All rights reserved.
+//
+
+import UIKit
+
+class PWViewController: UIViewController {
+
+    @IBOutlet weak var firstPassword: UITextField!
+    @IBOutlet weak var secondPassword: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+}
