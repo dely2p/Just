17075df41c6bce60b0955a37397ef375d66e401c
dely2p <lhyal2p@gmail.com>
2018-10-21 02:55:47 +0900

    fix storyboard.main to separate make and result viewController

---
+++ RealityStone/RealityStone/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  RealityStone
+//
+//  Created by dely on 2018. 10. 21..
+//  Copyright © 2018년 dely. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var resultImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
